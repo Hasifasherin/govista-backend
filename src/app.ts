43ce924fbb68f689548792bc4c3b6b39087a23ef
hasifasherin <hasifasherincp@gmail.com>
2026-02-13 11:00:23 +0530

    updated for deploy

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
     credentials: true,
   })
 );app.use(express.json());
